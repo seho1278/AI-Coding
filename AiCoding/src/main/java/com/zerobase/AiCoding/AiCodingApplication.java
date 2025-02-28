658dc3355e9678b940c80fbc2cc667b5e36c2bbc
seho1278 <seho1278@naver.com>
2025-03-01 06:32:41 +0900

    setting project

---
+++ AiCoding/src/main/java/com/zerobase/AiCoding/AiCodingApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.AiCoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCodingApplication.class, args);
+	}
+
+}
---
+++ AiCoding/src/test/java/com/zerobase/AiCoding/AiCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.AiCoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
